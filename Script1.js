115a2103a5b2c9fc90b51c66e55962faf8a970ce
SinhPN20796 <41608750+SinhPN20796@users.noreply.github.com>
2018-07-25 17:02:02 +0700

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,22 @@
+﻿function show() {
+    var a = parseInt(prompt("nhập số từ 30 đến 300:"));
+    while (a >= 30 && a <= 300) {
+        if (a % 7 == 0 && a % 13 == 0) {
+            document.writeln("a-z");
+        }
+        else if (a % 7 == 0 ) {
+            document.writeln("abc");
+        }
+        else if (a % 13 == 0) {
+            document.writeln("xyz");
+        }
+        else
+        {
+            document.writeln("số " + a + " không phải là bội số của 7 và 13");
+        }
+        return a;
+        }
+    document.writeln("Nhap sai")
+    show();
+    }
+show();
